0dbc2da541742d8da0e3fc64f898bc837f29b0b2
gambit <gambit@CHANGE>
2021-08-17 21:37:07 +0200

    Fix the pawn tests

--- tests/pawn.test.js
+++ tests/pawn.test.js
@@ -5,24 +5,34 @@ test('Pawn #1 [move]', () => {
 	const board = new Board();
 	board.load('8/8/8/8/8/8/4P3/8 w - - 0 1');
 
-	expect(board.getMoves().length).toEqual(2);
-	expect(board.getMoves().filter(m => {
-		return m === Board.coord.e3 || Board.coord.e4;
-	}).length).toEqual(2);
+	[
+		{ from: Board.coord.e2, to: Board.coord.e3 },
+		{ from: Board.coord.e2, to: Board.coord.e4 },
+	].forEach(move => {
+		expect(board.getMoves().some(m => {
+			return JSON.stringify(m) === JSON.stringify(move)
+		})).toBe(true);
+	});
 
-	board.load('4k3/8/8/8/4p3/8/4P3/4K3 w - - 0 1');
+	board.load('8/8/8/8/4p3/8/4P3/8 w - - 0 1');
 
-	expect(board.getMoves().length).toEqual(1);
-	expect(board.getMoves().filter(m => {
-		return m === Board.coord.e3;
-	}).length).toEqual(1);
+	[
+		{ from: Board.coord.e2, to: Board.coord.e3 },
+	].forEach(move => {
+		expect(board.getMoves().some(m => {
+			return JSON.stringify(m) === JSON.stringify(move)
+		})).toBe(true);
+	});
 
 	board.load('8/8/8/8/8/4P3/8/8 w - - 0 1');
 
-	expect(board.getMoves().length).toEqual(1);
-	expect(board.getMoves().filter(m => {
-		return m === Board.coord.e4;
-	}).length).toEqual(1);
+	[
+		{ from: Board.coord.e3, to: Board.coord.e4 },
+	].forEach(move => {
+		expect(board.getMoves().some(m => {
+			return JSON.stringify(m) === JSON.stringify(move)
+		})).toBe(true);
+	});
 
 	board.load('8/8/8/8/8/4p3/4P3/8 w - - 0 1');
 
@@ -33,38 +43,53 @@ test('Pawn #2 [capture]', () => {
 	const board = new Board();
 	board.load('8/8/8/8/3p1p2/4P3/8/8 w - - 0 1');
 
-	expect(board.getMoves().length).toEqual(3);
-	expect(board.getMoves().filter(m => {
-		return m === Board.coord.d4 || Board.coord.e4 || Board.coord.f4;
-	}).length).toEqual(3);
+	[
+		{ from: Board.coord.e3, to: Board.coord.d4 },
+		{ from: Board.coord.e3, to: Board.coord.e4 },
+		{ from: Board.coord.e3, to: Board.coord.f4 },
+	].forEach(move => {
+		expect(board.getMoves().some(m => {
+			return JSON.stringify(m) === JSON.stringify(move)
+		})).toBe(true);
+	});
 });
 
 test('Pawn #2 [en passant]', () => {
 	const board = new Board();
 	board.load('8/8/8/4Pp2/8/8/8/8 w - f6 0 1');
 
-	expect(board.getMoves().length).toEqual(2);
-	expect(board.getMoves().filter(m => {
-		return m === Board.coord.f6 || Board.coord.e6;
-	}).length).toEqual(2);
+	[
+		{ from: Board.coord.e5, to: Board.coord.e6 },
+		{ from: Board.coord.e5, to: Board.coord.f6 },
+	].forEach(move => {
+		expect(board.getMoves().some(m => {
+			return JSON.stringify(m) === JSON.stringify(move)
+		})).toBe(true);
+	});
 });
 
 test('Pawn #3 [capture border left]', () => {
 	const board = new Board();
 	board.load('8/8/8/8/7p/pp6/P7/8 w - - 0 1');
 
-	expect(board.getMoves().length).toEqual(1);
-	expect(board.getMoves().filter(m => {
-		return m === Board.coord.b3;
-	}).length).toEqual(1);
+	[
+		{ from: Board.coord.a2, to: Board.coord.b3 },
+	].forEach(move => {
+		expect(board.getMoves().some(m => {
+			return JSON.stringify(m) === JSON.stringify(move)
+		})).toBe(true);
+	});
 });
 
 test('Pawn #4 (capture border right)', () => {
 	const board = new Board();
 	board.load('8/8/8/8/8/6pp/p6P/8 w - - 0 1');
 
-	expect(board.getMoves().length).toEqual(1);
-	expect(board.getMoves().filter(m => {
-		return m === Board.coord.g3;
-	}).length).toEqual(1);
+	[
+		{ from: Board.coord.h2, to: Board.coord.g3 },
+	].forEach(move => {
+		expect(board.getMoves().some(m => {
+			return JSON.stringify(m) === JSON.stringify(move)
+		})).toBe(true);
+	});
 });
