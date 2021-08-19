72c4b57d83c00b2080d0ef72e8262e0c72bc0d3c
gambit <gambit@CHANGE>
2021-08-19 21:27:21 +0200

    Add queen movement and tests

--- src/MoveGenerator.ts
+++ src/MoveGenerator.ts
@@ -72,7 +72,7 @@ export default class MoveGenerator {
 			},
 			// --- Bishop ---
 			[3]: (square: number, activeColor: number): Array<SimpleMoves> => {
-				let targets: Array<SimpleMoves> = [];
+				const targets: Array<SimpleMoves> = [];
 				const directions: Array<any> = [
 					{
 						dir: -9,
@@ -119,7 +119,7 @@ export default class MoveGenerator {
 			},
 			// --- Rook ---
 			[4]: (square: number, activeColor: number): Array<SimpleMoves> => {
-				let targets: Array<SimpleMoves> = [];
+				const targets: Array<SimpleMoves> = [];
 				const directions: Array<any> = [
 					{
 						dir: -8,
@@ -154,10 +154,66 @@ export default class MoveGenerator {
 			},
 			// --- Queen ---
 			[5]: (square: number, activeColor: number): Array<SimpleMoves> => {
-				let targets: Array<number> = [];
-				const directions: Array<number> = [-9, -8, -7, -1, 1, 7, 8, 9];
+				const targets: Array<SimpleMoves> = [];
+				const directions: Array<any> = [
+					{
+						dir: -8,
+						abs: Math.abs(Board.getOffsetTop(square)),
+					},
+					{
+						dir: -1,
+						abs: Math.abs(Board.getOffsetLeft(square)),
+					},
+					{
+						dir: 1,
+						abs: Board.getOffsetRight(square),
+					},
+					{
+						dir: 8,
+						abs: Board.getOffsetBottom(square),
+					},
+					{
+						dir: -9,
+						abs: Math.min(
+							Math.abs(Board.getOffsetLeft(square)),
+							Math.abs(Board.getOffsetTop(square))
+						),
+					},
+					{
+						dir: -7,
+						abs: Math.min(
+							Math.abs(Board.getOffsetTop(square)),
+							Board.getOffsetRight(square)
+						),
+					},
+					{
+						dir: 7,
+						abs: Math.min(
+							Math.abs(Board.getOffsetLeft(square)),
+							Board.getOffsetBottom(square)
+						),
+					},
+					{
+						dir: 9,
+						abs: Math.min(
+							Board.getOffsetBottom(square),
+							Board.getOffsetRight(square)
+						),
+					},
+				];
 
-				return [];
+				for (var i = 0; i < directions.length; i++) {
+					for (var j = 0; j < directions[i].abs; j++) {
+						const targetSquare = square + directions[i].dir * (j + 1);
+						if (Piece.GetColor(this.board.squares[targetSquare]) === activeColor) {
+							break;
+						}
+
+						targets.push({ from: square, to: targetSquare });
+					}
+				}
+
+				return targets;
 			},
 			// --- King ---
 			[6]: (square: number, activeColor: number): Array<SimpleMoves> => {
---
+++ tests/queen.test.js
@@ -0,0 +1,166 @@
+import { expect, test } from '@jest/globals';
+import Board from '../src/Board';
+
+test('Queen #1 [move]', () => {
+	const board = new Board();
+	board.load('8/8/8/8/8/8/8/Q7 w - - 0 1');
+	let moves = board.getMoves();
+
+	[
+		{ from: Board.coord.a1, to: Board.coord.h8 },
+		{ from: Board.coord.a1, to: Board.coord.g7 },
+		{ from: Board.coord.a1, to: Board.coord.f6 },
+		{ from: Board.coord.a1, to: Board.coord.e5 },
+		{ from: Board.coord.a1, to: Board.coord.d4 },
+		{ from: Board.coord.a1, to: Board.coord.c3 },
+		{ from: Board.coord.a1, to: Board.coord.b2 },
+		{ from: Board.coord.a1, to: Board.coord.b1 },
+		{ from: Board.coord.a1, to: Board.coord.c1 },
+		{ from: Board.coord.a1, to: Board.coord.d1 },
+		{ from: Board.coord.a1, to: Board.coord.e1 },
+		{ from: Board.coord.a1, to: Board.coord.f1 },
+		{ from: Board.coord.a1, to: Board.coord.g1 },
+		{ from: Board.coord.a1, to: Board.coord.h1 },
+		{ from: Board.coord.a1, to: Board.coord.a2 },
+		{ from: Board.coord.a1, to: Board.coord.a3 },
+		{ from: Board.coord.a1, to: Board.coord.a4 },
+		{ from: Board.coord.a1, to: Board.coord.a5 },
+		{ from: Board.coord.a1, to: Board.coord.a6 },
+		{ from: Board.coord.a1, to: Board.coord.a7 },
+		{ from: Board.coord.a1, to: Board.coord.a8 },
+	].forEach(move => {
+		expect(moves.some(m => {
+			return JSON.stringify(m) === JSON.stringify(move);
+		})).toBe(true);
+	});
+
+	board.load('7Q/8/8/8/8/8/8/8 w - - 0 1');
+	moves = board.getMoves();
+
+	[
+		{ from: Board.coord.h8, to: Board.coord.a8 },
+		{ from: Board.coord.h8, to: Board.coord.b8 },
+		{ from: Board.coord.h8, to: Board.coord.c8 },
+		{ from: Board.coord.h8, to: Board.coord.d8 },
+		{ from: Board.coord.h8, to: Board.coord.e8 },
+		{ from: Board.coord.h8, to: Board.coord.f8 },
+		{ from: Board.coord.h8, to: Board.coord.g8 },
+		{ from: Board.coord.h8, to: Board.coord.g7 },
+		{ from: Board.coord.h8, to: Board.coord.f6 },
+		{ from: Board.coord.h8, to: Board.coord.e5 },
+		{ from: Board.coord.h8, to: Board.coord.d4 },
+		{ from: Board.coord.h8, to: Board.coord.c3 },
+		{ from: Board.coord.h8, to: Board.coord.b2 },
+		{ from: Board.coord.h8, to: Board.coord.a1 },
+		{ from: Board.coord.h8, to: Board.coord.h7 },
+		{ from: Board.coord.h8, to: Board.coord.h6 },
+		{ from: Board.coord.h8, to: Board.coord.h5 },
+		{ from: Board.coord.h8, to: Board.coord.h4 },
+		{ from: Board.coord.h8, to: Board.coord.h3 },
+		{ from: Board.coord.h8, to: Board.coord.h2 },
+		{ from: Board.coord.h8, to: Board.coord.h1 },
+	].forEach(move => {
+		expect(moves.some(m => {
+			return JSON.stringify(m) === JSON.stringify(move);
+		})).toBe(true);
+	});
+
+	board.load('8/8/8/3Q4/8/8/8/8 w - - 0 1');
+	moves = board.getMoves();
+
+	[
+		{ from: Board.coord.d5, to: Board.coord.a8 },
+		{ from: Board.coord.d5, to: Board.coord.b7 },
+		{ from: Board.coord.d5, to: Board.coord.c6 },
+		{ from: Board.coord.d5, to: Board.coord.a5 },
+		{ from: Board.coord.d5, to: Board.coord.b5 },
+		{ from: Board.coord.d5, to: Board.coord.c5 },
+		{ from: Board.coord.d5, to: Board.coord.a2 },
+		{ from: Board.coord.d5, to: Board.coord.b3 },
+		{ from: Board.coord.d5, to: Board.coord.c4 },
+		{ from: Board.coord.d5, to: Board.coord.e4 },
+		{ from: Board.coord.d5, to: Board.coord.f3 },
+		{ from: Board.coord.d5, to: Board.coord.g2 },
+		{ from: Board.coord.d5, to: Board.coord.h1 },
+		{ from: Board.coord.d5, to: Board.coord.e5 },
+		{ from: Board.coord.d5, to: Board.coord.f5 },
+		{ from: Board.coord.d5, to: Board.coord.g5 },
+		{ from: Board.coord.d5, to: Board.coord.h5 },
+		{ from: Board.coord.d5, to: Board.coord.e6 },
+		{ from: Board.coord.d5, to: Board.coord.f7 },
+		{ from: Board.coord.d5, to: Board.coord.g8 },
+		{ from: Board.coord.d5, to: Board.coord.d6 },
+		{ from: Board.coord.d5, to: Board.coord.d7 },
+		{ from: Board.coord.d5, to: Board.coord.d8 },
+	].forEach(move => {
+		expect(moves.some(m => {
+			return JSON.stringify(m) === JSON.stringify(move);
+		})).toBe(true);
+	});
+});
+
+test('Queen #2 [capture]', () => {
+	const board = new Board();
+	board.load('8/3p1p2/8/3Q4/8/8/8/8 w - - 0 1');
+	const moves = board.getMoves();
+
+	[
+		{ from: Board.coord.d5, to: Board.coord.a8 },
+		{ from: Board.coord.d5, to: Board.coord.b7 },
+		{ from: Board.coord.d5, to: Board.coord.c6 },
+		{ from: Board.coord.d5, to: Board.coord.a5 },
+		{ from: Board.coord.d5, to: Board.coord.b5 },
+		{ from: Board.coord.d5, to: Board.coord.c5 },
+		{ from: Board.coord.d5, to: Board.coord.a2 },
+		{ from: Board.coord.d5, to: Board.coord.b3 },
+		{ from: Board.coord.d5, to: Board.coord.c4 },
+		{ from: Board.coord.d5, to: Board.coord.e4 },
+		{ from: Board.coord.d5, to: Board.coord.f3 },
+		{ from: Board.coord.d5, to: Board.coord.g2 },
+		{ from: Board.coord.d5, to: Board.coord.h1 },
+		{ from: Board.coord.d5, to: Board.coord.e5 },
+		{ from: Board.coord.d5, to: Board.coord.f5 },
+		{ from: Board.coord.d5, to: Board.coord.g5 },
+		{ from: Board.coord.d5, to: Board.coord.h5 },
+		{ from: Board.coord.d5, to: Board.coord.e6 },
+		{ from: Board.coord.d5, to: Board.coord.f7 },
+		{ from: Board.coord.d5, to: Board.coord.d6 },
+		{ from: Board.coord.d5, to: Board.coord.d7 },
+	].forEach(move => {
+		expect(moves.some(m => {
+			return JSON.stringify(m) === JSON.stringify(move);
+		})).toBe(true);
+	});
+});
+
+test('Queen #3 [blocked]', () => {
+	const board = new Board();
+	board.load('8/3p1p2/3P4/3Q4/3P4/8/8/8 w - - 0 1');
+	const moves = board.getMoves();
+
+	[
+		{ from: Board.coord.d5, to: Board.coord.a8 },
+		{ from: Board.coord.d5, to: Board.coord.b7 },
+		{ from: Board.coord.d5, to: Board.coord.c6 },
+		{ from: Board.coord.d5, to: Board.coord.a5 },
+		{ from: Board.coord.d5, to: Board.coord.b5 },
+		{ from: Board.coord.d5, to: Board.coord.c5 },
+		{ from: Board.coord.d5, to: Board.coord.a2 },
+		{ from: Board.coord.d5, to: Board.coord.b3 },
+		{ from: Board.coord.d5, to: Board.coord.c4 },
+		{ from: Board.coord.d5, to: Board.coord.e4 },
+		{ from: Board.coord.d5, to: Board.coord.f3 },
+		{ from: Board.coord.d5, to: Board.coord.g2 },
+		{ from: Board.coord.d5, to: Board.coord.h1 },
+		{ from: Board.coord.d5, to: Board.coord.e5 },
+		{ from: Board.coord.d5, to: Board.coord.f5 },
+		{ from: Board.coord.d5, to: Board.coord.g5 },
+		{ from: Board.coord.d5, to: Board.coord.h5 },
+		{ from: Board.coord.d5, to: Board.coord.e6 },
+		{ from: Board.coord.d5, to: Board.coord.f7 },
+	].forEach(move => {
+		expect(moves.some(m => {
+			return JSON.stringify(m) === JSON.stringify(move);
+		})).toBe(true);
+	});
+});
