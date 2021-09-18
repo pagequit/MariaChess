1a7e4fab0d3c4ff639d87052c1f8a05590845ae4
kolaflash <kolaflash@TREK>
2021-09-18 11:41:09 +0200

    Refactor King and fix the tests

--- src/MoveGenerator.ts
+++ src/MoveGenerator.ts
@@ -1,6 +1,5 @@
 import Board from './Board';
 import Piece from './Piece';
-import Move from './Move';
 import SimpleMoves from './interfaces/SimpleMoves';
 
 import Pawn from './pieces/Pawn';
@@ -8,6 +7,7 @@ import Knight from './pieces/Knight';
 import Bishop from './pieces/Bishop';
 import Rook from './pieces/Rook';
 import Queen from './pieces/Queen';
+import King from './pieces/King';
 
 export default class MoveGenerator {
 	board: Board;
@@ -21,136 +21,8 @@ export default class MoveGenerator {
 			[Piece.Bishop]: Bishop,
 			[Piece.Rook]: Rook,
 			[Piece.Queen]: Queen,
-		}
-	// 		// --- King ---
-	// 		[6]: (square: number): Array<SimpleMoves> => {
-	// 			const targets: Array<SimpleMoves> = [];
-	// 			const directions: Array<any> = [
-	// 				{
-	// 					dir: -8,
-	// 					abs: Math.abs(Board.getOffsetTop(square)),
-	// 				},
-	// 				{
-	// 					dir: -1,
-	// 					abs: Math.abs(Board.getOffsetLeft(square)),
-	// 				},
-	// 				{
-	// 					dir: 1,
-	// 					abs: Board.getOffsetRight(square),
-	// 				},
-	// 				{
-	// 					dir: 8,
-	// 					abs: Board.getOffsetBottom(square),
-	// 				},
-	// 				{
-	// 					dir: -9,
-	// 					abs: Math.min(
-	// 						Math.abs(Board.getOffsetLeft(square)),
-	// 						Math.abs(Board.getOffsetTop(square))
-	// 					),
-	// 				},
-	// 				{
-	// 					dir: -7,
-	// 					abs: Math.min(
-	// 						Math.abs(Board.getOffsetTop(square)),
-	// 						Board.getOffsetRight(square)
-	// 					),
-	// 				},
-	// 				{
-	// 					dir: 7,
-	// 					abs: Math.min(
-	// 						Math.abs(Board.getOffsetLeft(square)),
-	// 						Board.getOffsetBottom(square)
-	// 					),
-	// 				},
-	// 				{
-	// 					dir: 9,
-	// 					abs: Math.min(
-	// 						Board.getOffsetBottom(square),
-	// 						Board.getOffsetRight(square)
-	// 					),
-	// 				},
-	// 			];
-
-	// 			const dirLength = directions.length;
-	// 			for (var i = 0; i < dirLength; i++) {
-	// 				for (var j = 0; j < directions[i].abs; j++) {
-	// 					const targetSquare = square + directions[i].dir * (j + 1);
-	// 					if (Piece.GetColor(this.board.squares[targetSquare]) === this.board.activeColor) {
-	// 						break;
-	// 					}
-
-	// 					targets.push({ from: square, to: targetSquare });
-	// 					break;
-	// 				}
-	// 			}
-
-	// 			return targets;
-	// 		},
-	// 	};
-	// 			// --- King ---
-	// 			[6]: (square: number): Array<SimpleMoves> => {
-	// 				const targets: Array<SimpleMoves> = [];
-	// 				const directions: Array<any> = [
-	// 					{
-	// 						dir: -8,
-	// 						abs: Math.abs(Board.getOffsetTop(square)),
-	// 					},
-	// 					{
-	// 						dir: -1,
-	// 						abs: Math.abs(Board.getOffsetLeft(square)),
-	// 					},
-	// 					{
-	// 						dir: 1,
-	// 						abs: Board.getOffsetRight(square),
-	// 					},
-	// 					{
-	// 						dir: 8,
-	// 						abs: Board.getOffsetBottom(square),
-	// 					},
-	// 					{
-	// 						dir: -9,
-	// 						abs: Math.min(
-	// 							Math.abs(Board.getOffsetLeft(square)),
-	// 							Math.abs(Board.getOffsetTop(square))
-	// 						),
-	// 					},
-	// 					{
-	// 						dir: -7,
-	// 						abs: Math.min(
-	// 							Math.abs(Board.getOffsetTop(square)),
-	// 							Board.getOffsetRight(square)
-	// 						),
-	// 					},
-	// 					{
-	// 						dir: 7,
-	// 						abs: Math.min(
-	// 							Math.abs(Board.getOffsetLeft(square)),
-	// 							Board.getOffsetBottom(square)
-	// 						),
-	// 					},
-	// 					{
-	// 						dir: 9,
-	// 						abs: Math.min(
-	// 							Board.getOffsetBottom(square),
-	// 							Board.getOffsetRight(square)
-	// 						),
-	// 					},
-	// 				];
-
-	// 				const dirLength = directions.length;
-	// 				for (var i = 0; i < dirLength; i++) {
-	// 					for (var j = 0; j < directions[i].abs; j++) {
-	// 						const targetSquare = square + directions[i].dir * (j + 1);
-	// 						targets.push({ from: square, to: targetSquare });
-	// 						break;
-	// 					}
-	// 				}
-
-	// 				return targets;
-	// 			},
-	// 	};
-	// }
+			[Piece.King]: King,
+		};
 	}
 
 	getCoveredSquaresFrom(square: number): Array<number> {
